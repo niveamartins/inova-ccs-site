a8aa7f1d0429b22f7b46e18b13cbc3783072306e
sidneyouteiro <sid@dcc.ufrj.br>
2020-03-10 16:03:20 -0300

    definindo o comportamento do get e post

--- backend/src/controller/endpoints.py
+++ backend/src/controller/endpoints.py
@@ -2,7 +2,22 @@ import json
 
 from flask import request, jsonify, Blueprints
 from src.model.laboratorio import laboratorio
+from src.util.mysql import estabelece_sessao
+
 
 @blueprint.route
 def index('/', methods=['GET']):
 
+    sessao = estabelece_sessao()
+    info = sessao.query(laboratorio).all()
+    info = [i.marks() for i in info]
+    sessao.close()
+    return jsonify(info)
+
+@blueprint.route
+def servico(('/servico?Tag=<id>', methods=['POST'])):
+    sessao = estabelece_sessao()
+    info = sessao.query(laboratorio).filter(laboratorio.Id == Tag).one()
+    info = [i.servicos() for i in info]
+    sessao.close()
+    return jsonify(info)

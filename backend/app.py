9724815aede166e1aa4969b4e22b1f4f3c28ffb5
sidneyouteiro <sid@dcc.ufrj.br>
2020-03-09 23:58:54 -0300

    uma nova esperança

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,10 @@
 from flask import Flask, request, jsonify
-import mysql.connector
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'root:123456@localhost:5002/database'
-db = SQLAlchemy(app)
-
-class marcadores(db.Model):
-    Id = db.Column(db.Integer, primary_key=True)
-    Coordenada_S = db.Column(db.Float(precision=10,asdecimal=True))
-    Coordenada_W = db.Column(db.Float(precision=10,asdecimal=True))
-
-    def __repr__(self):
-        return '<id %r>' % self.Id
-
-#@app.route('/', methods=['GET','POST'])
-#def index():
- #   if request.method == 'POST':
- #       request.form
-
-
-
-
-
-#def get_marcador(id):
-
+from sqlalchemy import S
+from flask_cors import CORS
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1')
+    app = Flask(__name__)
+    CORS(app)
+    app.config['JSON_AS_ASCII'] = False
+
+    app.run(port=8081)

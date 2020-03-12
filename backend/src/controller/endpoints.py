cc243eb26616e9ea56e79685db39100fd5b76496
Nivea <Niveamartins@dcc.ufrj.br>
2020-03-12 11:20:08 -0300

    tirando os print nos endpoints.py

--- backend/src/controller/endpoints.py
+++ backend/src/controller/endpoints.py
@@ -13,14 +13,12 @@ def index():
     info = sessao.query(laboratorio).all()
     info = [i.marks() for i in info]
     sessao.close()
-    print("foi")
     return jsonify(info)
 
 @blueprint.route('/servico', methods=['GET'])
 def servico():
     sessao = estabelece_sessao()
     Tag = request.args.get('Tag')
-    print(Tag)
     info = sessao.query(laboratorio).filter(laboratorio.Id == Tag).all()
     info = [i.servicos() for i in info]
     sessao.close()

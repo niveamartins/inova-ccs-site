4a11a677119bdf3dd869e63b5bf719d1bac831bd
niveamartins <42484149+niveamartins@users.noreply.github.com>
2020-03-22 00:55:38 -0300

    Configuração do env para o front

--- frontend/src/app/labs.service.ts
+++ frontend/src/app/labs.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LabsService {
-  backendURL = 'http://localhost:8081/';
-
   constructor(public http: HttpClient) { }
 
   // função que pega todos os labs
   public getLabs(): Observable<any> {
-    return this.http.get(this.backendURL);
+    return this.http.get(environment.myEndpoint);
   }
   // servico?Tag=1
   public getLab_byId(id: string): Observable<any> {
-    return this.http.get(this.backendURL + 'servico?Tag=' + id);
+    return this.http.get(environment.myEndpoint + 'servico?Tag=' + id);
   }
 }
